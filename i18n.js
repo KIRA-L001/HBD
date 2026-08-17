975bad7007666bbd4a3f188619457ce070f25ef6
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-17 18:12:24 +0530

    feat(hbd): add multi-language birthday greetings i18n module

--- i18n.js
+++ i18n.js
@@ -1,1 +1,24 @@
-# i18n
+// i18n
+(function (root) {
+  'use strict';
+
+  const HBD_I18N = {
+    en: 'Happy Birthday!',
+    es: '¡Feliz Cumpleaños!',
+    fr: 'Joyeux Anniversaire!',
+    de: 'Alles Gute zum Geburtstag!',
+    hi: 'जन्मदिन मुबारक हो!',
+    ja: 'お誕生日おめでとう!',
+  };
+
+  function hbdTranslate(lang) {
+    return HBD_I18N[lang] || HBD_I18N.en;
+  }
+
+  root.HBD_I18N = HBD_I18N;
+  root.hbdTranslate = hbdTranslate;
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { HBD_I18N, hbdTranslate };
+  }
+})(typeof window !== 'undefined' ? window : this);
